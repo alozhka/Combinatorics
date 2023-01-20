30589baf02e553ab6a5b1fd9fa0eb099fa2b28d7
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2023-01-20 13:05:47 +0300

    Сделал 1 задание с алгоритмом Найраны

---
+++ lw1/Nairana'sAlgorithm/Nairana'sAlgorithm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <boost/timer.hpp>
+
+using namespace std;
+
+int main()
+{
+    vector<size_t> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    boost::timer t;
+    t.restart();
+    do 
+    {
+        /*
+        copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
+        cout << endl;
+        */
+    } while ( next_permutation(v.begin(), v.end()) );
+    double duration = t.elapsed();
+
+    cout << duration << endl;
+}
