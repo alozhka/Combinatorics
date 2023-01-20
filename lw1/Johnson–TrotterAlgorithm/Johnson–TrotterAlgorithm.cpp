99e8600ce82f386c3ca3d0c1765facb038b98020
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2023-01-20 22:55:57 +0300

    Дописал функционал алгоритма Джонсона-Троттера

--- lw1/Johnson–TrotterAlgorithm/Johnson–TrotterAlgorithm.cpp
+++ lw1/Johnson–TrotterAlgorithm/Johnson–TrotterAlgorithm.cpp
@@ -1,34 +1,51 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
 #include <boost/timer.hpp>
 
 using namespace std;
 
 int main()
 {
-    vector<size_t> v =    { 8, 1,  2,  3,  4,  5,  6,  7,  8 };  // текущий
-    vector<size_t> p = v;                                                     // обратный
-    vector<int> d =       { 0, 0, -1, -1, -1, -1, -1, -1 };      // направление
+    vector<size_t> v,p; // обычный, обратный
+    vector<int> d;      // направления сдвига
     boost::timer t;
+    size_t in, cur;
 
-    size_t cur = 11;
+    setlocale(LC_ALL, "rus");
+    cout << "Введите количество элементов в перестановке: ";
+    cin >> in;
+    cout << endl;
+
+    v.push_back(in + 1);
+    p.push_back(0); // занимаем нулевые ячейки
+    d.push_back(0);
+    for (int i = 1; i <= in; i++)
+    {
+        v.push_back(i);
+        p.push_back(i);
+        d.push_back(-1);
+    }
+    d[1] = 0;
+    v.push_back(in + 1);
+    cur = in + 1;
+
     t.restart();
     while (cur != 1)
     {
-        copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
+        
+        copy(v.begin() + 1, v.end() - 1, ostream_iterator<size_t>(cout, " "));
         cout << endl;
+        
 
-        cur = 7;
+        cur = in;
         while (v[ p[cur] + d[cur] ] > cur)
         {
             d[cur] = -d[cur];
             cur -= 1;
         }
-        // замена текущей позиции с позицией с учётом сдвига
-        swap(  v[p[cur]], v[ p[cur] + d[cur] ]  );
-        // это значение в -1 степени
-        swap( p[  v[ p[cur] ]  ], p[cur]);
+        swap(  v[p[cur]], v[ p[cur] + d[cur] ]  );  // замена текущей позиции с позицией с учётом сдвига
+        swap( p[  v[ p[cur] ]  ], p[cur] );          // это значение в -1 степени
     }
-    cout << t.elapsed() << endl;
+    double duration = t.elapsed();
+    cout << duration << endl;
 }
