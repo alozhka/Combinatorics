333fcfc945fa5d3c92ce11a017e4342aa7e5d450
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2023-02-08 23:30:30 +0300

    Начал писать рюкзак через генерацию сочетаний
    Ввод работает

--- lw2/KnapsackProblem/KnapsackProblem.cpp
+++ lw2/KnapsackProblem/KnapsackProblem.cpp
@@ -1,67 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <unordered_map>
-#include <climits>
 using namespace std;
 
-// Значения (хранятся в массиве `v`)
-// Веса (хранятся в массиве `w`)
-// Общее количество различных элементов `n`
-// Вместимость рюкзака `W`
-int knapsack(int v[], int w[], int n, int W, unordered_map<string, int> lookup)
+struct item
 {
-    // базовый вариант: отрицательная емкость
-    if (W < 0) {
-        return INT_MIN;
-    }
+    size_t weight, cost;
+};
 
-    // базовый случай: элементов не осталось или вместимость становится равной 0
-    if (n < 0 || W == 0) {
-        return 0;
-    }
+void knapsack(vector<item> &v, size_t max_cost, size_t max_weight)
+{
 
-    // создаем уникальный ключ карты из динамических элементов ввода
-    string key = to_string(n) + "|" + to_string(W);
-
-    // если подзадача видится впервые, решаем ее и
-    // сохраняем результат на карту
-    if (lookup.find(key) == lookup.end())
-    {
-        // Случай 1. Включить текущий предмет `v[n]` в рюкзак и повторить для
-        // оставшиеся элементы `n-1` с уменьшенной вместимостью `W-w[n]`
-        int include = v[n] + knapsack(v, w, n - 1, W - w[n], lookup);
-
-        // Случай 2. Исключить текущий предмет `v[n]` из рюкзака и повторить для
-        // оставшиеся элементы `n-1`
-        int exclude = knapsack(v, w, n - 1, W, lookup);
-
-        // присвоить максимальное значение, которое мы получаем, включая или исключая текущий элемент
-        lookup[key] = max(include, exclude);
-    }
-
-    // возвращаем решение текущей подзадачи
-    return lookup[key];
 }
 
 // 0–1 Задача о рюкзаке
 int main()
 {
-    vector <size_t> V;
-    // ввод: набор элементов, каждый со своим весом и значением
-    int v[] = { 20, 5, 10, 40, 15, 25 };
-    int w[] = { 1, 2, 3, 8, 7, 4 };
+    /**
+    * n - количество элементов
+    * S - максимальную стоимость
+    * T - максимальный вес
+    */
+    size_t n, S, T;
+    setlocale(LC_ALL, "rus");
 
-    // вместимость рюкзака
-    int W = 10;
+    cout << "Введите количество элементов, максимальную стоимость и максимальный вес: ";
+    cin >> n >> S >> T;
+    vector <item> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\nВведите вес и стоимость " << i + 1 << " элемента: ";
+        cin >> v[i].weight >> v[i].cost;
+    }
 
-    // общее количество предметов
-    int n = sizeof(v) / sizeof(v[0]);
-
-    // создаем карту для хранения решений подзадачи
-    unordered_map<string, int> lookup;
-
-    cout << "Knapsack value is " << knapsack(v, w, n - 1, W, lookup);
+    knapsack(v, S, T);
 
     return 0;
 }
