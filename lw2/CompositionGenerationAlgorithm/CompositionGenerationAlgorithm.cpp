ae49e18a3224e7b750f0b2a514202cdf77e8b359
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2023-02-08 19:58:25 +0300

    Сделал задачу с порождением композиции

--- lw2/CompositionGenerationAlgorithm/CompositionGenerationAlgorithm.cpp
+++ lw2/CompositionGenerationAlgorithm/CompositionGenerationAlgorithm.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 
 using namespace std;
 
@@ -9,7 +8,7 @@ size_t findMinIndex(vector<size_t> &v)
 {
     size_t indexmin = 0, min = v[0];
 
-    for (int i = 0; i < v.size(); i++)
+    for (int i = 0; i < v.size() - 1; i++)
     {
         if (v[i] < min)
         {
@@ -21,8 +20,9 @@ size_t findMinIndex(vector<size_t> &v)
     return indexmin;
 }
 
-void createPermutation(vector<size_t> &v)
+void createPermutation(vector<size_t> v)
 {
+    sort(v.begin(), v.end());
     do
     {
         copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
@@ -30,56 +30,34 @@ void createPermutation(vector<size_t> &v)
     } while (next_permutation(v.begin(), v.end()));
 }
 
-void splitLastIndexes(vector<size_t> &v, size_t currentIndex)
-{
-    size_t sum = 0;
-    for (auto &n : v)
-        sum += n;
-}
-
-void createCompositions(size_t size)
+void createCompositions(ostream& ostr, size_t size)
 {
     vector<size_t> v;
     size_t curr = 0;
-    bool isFirst = true;
 
     // инициализация
     for (int i = 0; i < size; i++)
     {
         v.push_back(1);
     }
-    copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
-    cout << endl;
+    copy(v.begin(), v.end(), ostream_iterator<size_t>(ostr, " "));
+    ostr << endl;
 
     while (v.size() > 1)
     {
         // ищем самый левый минимальный индекс
         curr = findMinIndex(v);
 
-        // суммируем элементы и удаляем ненужный
-        if (curr < v.size() - 1)
+        v[curr]++;
+        if (--v.back() == 0)
         {
-            v[curr] += v[curr + 1];
-            v.erase(v.begin() + curr + 1);
-        }
-        else
-        {
-            v[curr - 1] += v[curr];
-            v.erase(v.begin() + curr);
+            v.pop_back();
         }
 
-        // костыль для норамльной начальной генерации
-        if (isFirst)
-        {
-            reverse(v.begin(), v.end());
-            isFirst = false;
-        }
-
-        splitLastIndexes(v, curr);
         // генерируем перестановки
         createPermutation(v);
     }
-    cout << endl;
+    ostr << endl;
 }
 
 
@@ -92,6 +70,6 @@ int main()
 	cin >> size;
     cout << endl;
 
-    createCompositions(size);
+    createCompositions(cout, size);
     cout << endl;
 }
