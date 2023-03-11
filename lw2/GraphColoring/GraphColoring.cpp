f2c610136aa9da24586e6966fb496befcb90d209
SergeyLozhkin <100339481+alozhka@users.noreply.github.com>
2023-03-11 16:13:00 +0300

    Добавил проверку неудачной обработки строки

--- lw2/GraphColoring/GraphColoring.cpp
+++ lw2/GraphColoring/GraphColoring.cpp
@@ -1,32 +1,44 @@
 ﻿#include <iostream>
+#include <optional>
 #include "Graph.hpp"
 
 using namespace std;
 
-string getData()
+optional<string> parseCmd(const int argc, char* argv[])
 {
 	string source;
 
-	cout << "Enter file name: ";
-	cin >> source;
-	cout << endl;
-
-	return source;
+	if (argc != 2)
+	{
+		cout << "Invalid input data\n";
+		return nullopt;
+	}
+	else
+	{
+		return argv[1];
+	}
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	AdjMatrix adj;
-	string dest = getData();
+	auto dest = parseCmd(argc, argv);
 	 
-	if (!initMatrix(adj, dest))
+	if (!dest)
 	{
 		return 1;
 	}
 	else
 	{
-		graphColoring(adj);
+		if (initMatrix(adj, *dest))
+		{
+			graphColoring(adj);
+		}
+		else
+		{
+			return 1;
+		}
 	}
 
 	return 0;
